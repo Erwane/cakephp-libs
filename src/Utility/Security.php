780efe95bd8afb837e0a80adf61cb181af8ced02
Erwane Breton <erwane@phea.fr>
2019-06-03 15:48:22 +0200

    Security::token use parent randomBytes

--- src/Utility/Security.php
+++ src/Utility/Security.php
@@ -7,9 +7,9 @@ class Security extends CakeSecurity
 {
     public function token(int $length = 8)
     {
-        $random = base64_encode(openssl_random_pseudo_bytes($length * 4));
+        $random = base64_encode(parent::randomBytes($length * 4));
         $clean = preg_replace('/[^A-Za-z0-9]/', '', $random);
 
-        return substr($clean, mt_rand(1, $length * 2), $length);
+        return substr($clean, random_int(1, $length * 2), $length);
     }
 }
