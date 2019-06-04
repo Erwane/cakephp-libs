f433d9100a8e490391326c52f4ef31e126d6e9c2
Erwane Breton <erwane@phea.fr>
2019-06-04 13:43:33 +0200

    Security short and alpha hash

--- src/Utility/Security.php
+++ src/Utility/Security.php
@@ -12,4 +12,40 @@ class Security extends CakeSecurity
 
         return substr($clean, random_int(1, $length * 2), $length);
     }
+
+    /**
+     * short hash of input
+     * @param  mixed $input input
+     * @return string
+     */
+    public static function shortHash($input)
+    {
+        if (is_array($input)) {
+            $input = serialize($input);
+        }
+
+        return substr(parent::hash($input, 'sha1'), 3, 16);
+    }
+
+    /**
+     * unique alphanumerique hash
+     * /!\ VERY SLOW /!\
+     * @param  string|array $input inpu
+     * @return string 16 chars
+     */
+    public static function alphaHash($input)
+    {
+        if (is_array($input)) {
+            $input = serialize($input);
+        }
+
+        $hash = password_hash($input, PASSWORD_BCRYPT, ['salt' => 'LpRjhRVpjD18lbQBNWcvQHeBtg8f9Z5n']);
+
+        $hash = str_replace('$2y$10$', '', $hash);
+        $hash = str_replace('.', '', $hash);
+        $hash = str_replace('/', '', $hash);
+
+        // return hash without key
+        return substr($hash, 22, 16);
+    }
 }
